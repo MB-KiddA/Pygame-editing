46e6d6b94f87ade8c146f31a68329bd4e94274dc
Goofy Goober <91341620+MB-KiddA@users.noreply.github.com>
2023-06-06 12:45:57 -0700

    changed to make more accurate
    Changed yellow to green

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def red_handle_movement(keys_pressed, red):
 
 def handle_bullets(yellow_bullets, red_bullets, yellow, red):
     for bullet in yellow_bullets:
-        bullet.y -= 7
+        bullet.y -= BULLET_VEL
         if red.colliderect(bullet):
             pygame.event.post(pygame.event.Event(RED_HIT))
             yellow_bullets.remove(bullet)
@@ -108,7 +108,7 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
             yellow_bullets.remove(super)
 
     for bullet in red_bullets:
-        bullet.y += 7
+        bullet.y += BULLET_VEL
         if yellow.colliderect(bullet):
             pygame.event.post(pygame.event.Event(YELLOW_HIT))
             red_bullets.remove(bullet)
