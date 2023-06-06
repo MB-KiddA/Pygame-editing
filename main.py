fef08ea5dca1e563e45aeb0bff5e265d1cf8fd6a
Goofy Goober <91341620+MB-KiddA@users.noreply.github.com>
2023-06-06 09:02:56 -0700

    Update main.py

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def red_handle_movement(keys_pressed, red):
 
 def handle_bullets(yellow_bullets, red_bullets, yellow, red):
     for bullet in yellow_bullets:
-        bullet.y += BULLET_VEL
+        bullet.y -= BULLET_VEL
         if red.colliderect(bullet):
             pygame.event.post(pygame.event.Event(RED_HIT))
             yellow_bullets.remove(bullet)
@@ -98,7 +98,7 @@ def handle_bullets(yellow_bullets, red_bullets, yellow, red):
             yellow_bullets.remove(bullet)
 
     for bullet in red_bullets:
-        bullet.x -= BULLET_VEL
+        bullet.y += BULLET_VEL
         if yellow.colliderect(bullet):
             pygame.event.post(pygame.event.Event(YELLOW_HIT))
             red_bullets.remove(bullet)
